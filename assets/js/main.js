83ef13dea48ca4f104c95dd9c279b8205ee12faa
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-06-03 18:34:58 +0700

    Loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,20 @@ var header = document.getElementById("navbar-list");
 var link = header.getElementsByClassName("navbar__link");
 
 for (var i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("navbar__item--active");
-  current[0].className = current[0].className.replace(" navbar__item--active", "");
-  this.className += " navbar__item--active";
+  link[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("navbar__item--active");
+    current[0].className = current[0].className.replace(" navbar__item--active", "");
+    this.className += " navbar__item--active";
   });
 }
+
+//Loading
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild("loader");
+  })
+})
